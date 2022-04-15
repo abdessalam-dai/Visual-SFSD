6fde06c05e22655c6bf06f50a6c5ed0f08e46934
abdessalam-dai <daihalo70@gmail.com>
2022-04-15 23:43:05 +0100

    Create file form steps are working

--- visual_sfsd/static/js/accounts/dashboard/DomElements.js
+++ visual_sfsd/static/js/accounts/dashboard/DomElements.js
@@ -1,4 +1,26 @@
 export const createFileBtn = document.querySelector("#create-file-btn");
-export const closeCreateFileModal = document.querySelector("#close-create-file-modal");
 export const createFileModalOverlay = document.querySelector("#create-file-modal-overlay");
 export const createFileModal = document.querySelector("#create-file-modal");
+
+export const step1 = document.querySelector("#step-1");
+export const step2 = document.querySelector("#step-2");
+export const step3Sequential = document.querySelector("#step-3-sequential");
+export const step3Indexed = document.querySelector("#step-3-indexed");
+export const step3Hashing = document.querySelector("#step-3-hashing");
+
+export const step4Sequential = document.querySelector("#step-4-sequential");
+
+export const maxNbEnregs = document.querySelector("#max-nb-enregs");
+export const maxNbBlocks = document.querySelector("#max-nb-blocks");
+export const dummyDataForm = document.querySelector("#dummy-data-form");
+export const fillWithDummyData = document.querySelector("#fill-with-dummy-data");
+export const elementsNumber = document.querySelector("#elements-number");
+export const minKey = document.querySelector("#min-key");
+export const maxKey = document.querySelector("#max-key");
+export const submitCreateFile = document.querySelector("#submit-create-file");
+
+export const fileName = document.querySelector("#file-name");
+export const submitFileNameBtn = document.querySelector("#submit-file-name-btn");
+
+export const fileAccessBtns = document.querySelectorAll("#file-access-btn");
+export const fileTypeBtns = document.querySelectorAll("#file-type-btn");
--- visual_sfsd/static/js/accounts/dashboard/index.js
+++ visual_sfsd/static/js/accounts/dashboard/index.js
@@ -1,12 +1,118 @@
-import * as DomElements from "./DomElements.js";
+import * as DE from "./DomElements.js";
+import {dummyDataForm, fillWithDummyData} from "./DomElements.js";
 
-DomElements.createFileBtn.addEventListener('click', function (e) {
-    DomElements.createFileModalOverlay.classList.remove('hidden');
-    DomElements.createFileModal.classList.remove('hidden');
+
+// form state
+let fName = "";
+let fAccess = "";
+let fType = "";
+
+
+const resetForm = () => {
+    // reset state
+    fName = "";
+    fAccess = "";
+    fType = "";
+
+    // change the visibility of the steps
+    DE.step1.classList.remove('hidden');
+    DE.step2.classList.add('hidden');
+    DE.step3Sequential.classList.add('hidden');
+    DE.step3Indexed.classList.add('hidden');
+    DE.step3Hashing.classList.add('hidden');
+    // hide overlay
+    DE.createFileModalOverlay.classList.add("hidden");
+    DE.createFileModal.classList.add('hidden');
+    DE.fileName.value = "";
+    DE.fileName.blur();
+}
+
+
+// handle click on the create file button
+DE.createFileBtn.addEventListener('click', function (e) {
+    DE.createFileModalOverlay.classList.remove('hidden');
+    DE.createFileModal.classList.remove('hidden');
+    DE.fileName.focus();
 });
 
-DomElements.createFileModalOverlay.addEventListener('click', function(e) {
-    console.log(this)
-    this.classList.add("hidden");
-    DomElements.createFileModal.classList.add('hidden');
+// handle click on the overlay (to close the modal)
+DE.createFileModalOverlay.addEventListener('click', function (e) {
+    resetForm();
 });
+
+// handle submitting the file name
+const handleFileNameSubmit = () => {
+    fName = DE.fileName.value.trim();
+
+    if (fName.length > 100 || fName.length === 0) { // make sure the length doesn't exceed 100, and has at least 1
+        DE.fileName.value = "";
+        DE.fileName.focus();
+    } else {
+        DE.step1.classList.add('hidden');
+        DE.step2.classList.remove('hidden');
+    }
+}
+DE.submitFileNameBtn.addEventListener('click', function (e) {
+    handleFileNameSubmit();
+});
+
+DE.fileName.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        handleFileNameSubmit();
+    }
+});
+
+// handle choosing file access
+Array.from(DE.fileAccessBtns).forEach((fileAccessBtn) => {
+    fileAccessBtn.addEventListener('click', function (e) {
+        fAccess = this.dataset.access;
+        switch (fAccess) {
+            case "sequential":
+                DE.step2.classList.add('hidden');
+                DE.step3Sequential.classList.remove('hidden');
+                break;
+            case "indexed":
+                DE.step2.classList.add('hidden');
+                DE.step3Indexed.classList.remove('hidden');
+                break;
+            case "hashing":
+                DE.step2.classList.add('hidden');
+                DE.step3Hashing.classList.remove('hidden');
+                break;
+            default:
+                DE.step2.classList.remove('hidden');
+        }
+    });
+});
+
+// handle choosing file type
+Array.from(DE.fileTypeBtns).forEach((fileTypeBtn) => {
+    fileTypeBtn.addEventListener('click', function (e) {
+        fType = this.dataset.type;
+        switch (fType) {
+            case "TOF":
+            case "TnOF":
+            case "LOF":
+            case "LnOF":
+                DE.step3Sequential.classList.add('hidden');
+                DE.step4Sequential.classList.remove('hidden');
+                break;
+            default:
+
+        }
+        console.log({fName, fAccess, fType});
+    });
+});
+
+
+
+// handle file configuration
+// handle clicking fill with dummy data
+DE.fillWithDummyData.addEventListener('change', function () {
+    if (this.checked) {
+        DE.dummyDataForm.classList.remove('hidden');
+    } else {
+        DE.dummyDataForm.classList.add('hidden');
+    }
+});
+
