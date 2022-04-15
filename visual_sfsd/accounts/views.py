428423d6e62a38b0ea9d06bc4d778fa07fc32dbb
louzanizineddine <louzanizineddine@gmail.com>
2022-04-15 02:17:26 +0100

    created the dashboard html

--- visual_sfsd/accounts/views.py
+++ visual_sfsd/accounts/views.py
@@ -7,3 +7,7 @@ def login(request):
 
 def register(request):
     return render(request, 'accounts/register/index.html')
+
+
+def dashboard(request):
+    return render(request, 'components/navbar/navbar_dashboard.html')
